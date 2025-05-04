cf9b0af9e6ff515b1f9e1278752f3df2b92ae35d
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-05-04 22:55:09 +0530

    Delete hotell/hotel directory

--- hotell/hotel/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Amenities)
-admin.site.register(Hotel)
-admin.site.register(HotelImages)
-admin.site.register(HotelBooking)
